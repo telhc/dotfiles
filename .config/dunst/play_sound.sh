e2fcd072669a3a20faf15b85807ab20545e02f0b
telhc <lhterryc@gmail.com>
2024-11-19 10:04:30 +1100

    Update dunst config

--- .config/dunst/play_sound.sh
+++ .config/dunst/play_sound.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-paplay /usr/share/sounds/freedesktop/stereo/message-new-instant.oga --volume=150000
+if [ "$(<$HOME/.cache/mute_notif)" = "0" ]; then
+  paplay /usr/share/sounds/freedesktop/stereo/message-new-instant.oga --volume=150000
+fi
